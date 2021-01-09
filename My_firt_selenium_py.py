96d9d75c746463fba306d158ca17390bcfe235f5
Rahul Bhapkar <rahul.bhapkar1@gmail.com>
2021-01-09 19:30:37 +0530

    This is my first python pytest file having 3 test cases

---
+++ My_firt_selenium_py.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+driver = webdriver.Chrome(executable_path="../resources/chromedriver.exe")
+driver.maximize_window()
+driver.get("http://selenium-examples.nichethyself.com")
+driver.find_element_by_id("loginname").send_keys("stc123")
+driver.find_element_by_id("loginpassword").send_keys("12345")
+driver.find_element_by_id("loginbutton").click()
+driver.close()
