750b619dbe2b6355db9817c36f8d49081ba2d890
rahulbhapk <77199702+rahulbhapk@users.noreply.github.com>
2021-01-28 13:27:24 +0530

    Commit changes

--- My_firt_selenium_py.py
+++ My_firt_selenium_py.py
@@ -5,4 +5,5 @@ driver.get("http://selenium-examples.nichethyself.com")
 driver.find_element_by_id("loginname").send_keys("stc123")
 driver.find_element_by_id("loginpassword").send_keys("12345")
 driver.find_element_by_id("loginbutton").click()
+#modify
 driver.close()
